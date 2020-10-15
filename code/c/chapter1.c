cd24265e19d30591f143613a72c2bf59df80813e
KaiFujimoto <kaifujidesu@gmail.com>
2020-10-15 11:56:25 -0400

    10-14-2020 C programming Chapter 1 Exercises 1-1 & 1-2

---
+++ code/c/chapter1.c
@@ -0,0 +1,21 @@
+/* problems from Chapter 1 */
+
+/* Exercise 1-1 */
+/* Run the "hello world" program on your system. Experiment with leaving out parts of the program, to see what error messages you get */
+#include <stdio.h>
+
+// main()
+// {
+//   printf("hello, world.\n");
+// }
+
+/* Exercise 1-2 */
+/* Experiment to find out what happens when printf's argument string contains \c, where c is some character not listed above. */
+
+// main()
+// {
+//   printf("hello, world. \c"); /* compiles without errors, but prints out 'hello, world. cc' with no \n afterwards */
+// }
+
+/* Exercise 1-3 */
+/* Modify the temperature conversion program to print a heading above the table */
